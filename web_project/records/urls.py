1b79b8142ce15f7cb64cf4b792bf1cc0ba9b5899
McLaren <artyudin@outlook.com>
2017-04-19 20:55:53 -0400

    Apr 19

--- web_project/records/urls.py
+++ web_project/records/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 	url(r'^$', views.PostListView.as_view(), name='list_posts'),
 	url(r'^(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='detail_posts'),
 	url(r'^create/$', views.CreatePostView.as_view(), name='create_posts'),
+	url(r'^(?P<pk>\d+)/update/$', views.UpdatePostView.as_view(), name='update_post'),
+    url(r'^(?P<pk>\d+)/delete/$', views.DeletePostView.as_view(), name='delete_post'),
+    
 ]
--- web_project/records/views.py
+++ web_project/records/views.py
@@ -3,6 +3,9 @@ from records.models import Post
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 
 
 
@@ -18,3 +21,31 @@ class PostDetailView(DetailView):
 class CreatePostView(CreateView):
 	model = Post
 	fields = ['title','slug','content', 'image']
+
+
+class UpdatePostView(UpdateView):
+	model = Post
+	fields =['title', 'slug','content', 'image']
+	template_name_suffix = '_update_form'
+
+
+
+class DeletePostView(DeleteView):
+	model = Post
+	success_url = reverse_lazy('records:list_posts')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
